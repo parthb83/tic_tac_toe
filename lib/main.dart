5c64e130bec98077a8261a5a856841816fba6b0a
parthb83 <parthbansal1536@gmail.com>
2021-06-11 17:54:15 +0530

    Basic design of home page

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.black),
-        home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text("Tic Tac Toe"),
-          ),
-          body: Container(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                "Let's Play Tic Tac Toe!!!",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
+      theme: ThemeData(primaryColor: Colors.black),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Tic Tac Toe"),
+        ),
+        body: Container(
+          width: 1500,
+          height: 2500,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/gamebgm2.jpeg"),
+              fit: BoxFit.cover,
             ),
           ),
-        ));
+          child: SizedBox(
+            width: 1500,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "Choose Your Mode",
+                    style:
+                        TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+                  ),
+                  padding: EdgeInsets.fromLTRB(0.0, 90.0, 0.0, 30.0),
+                ),
+                Container(
+                  width: 200,
+                  height: 80,
+                  child: ElevatedButton(
+                    child: Text("Single Player"),
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                    ),
+                  ),
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
+                ),
+                Container(
+                  width: 200,
+                  height: 80,
+                  child: ElevatedButton(
+                    child: Text("Double Player"),
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                    ),
+                  ),
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
